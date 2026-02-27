b5175e87507f81c751cbb753b0adce542394cceb
franco ayala <fa8050386@gmail.com>
2026-02-27 11:55:44 -0400

    Remove unused useEffect import

--- src/pages/FlowBuilder.tsx
+++ src/pages/FlowBuilder.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useMemo, useState, useEffect } from 'react';
+import { useCallback, useRef, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ReactFlow, {
     Background,
